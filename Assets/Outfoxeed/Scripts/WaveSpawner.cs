cb591c2ac65dddec7008f6c7dc6c23898c5f11d4
Morigan Letourneau <letourneaumorigan@gmail.com>
2020-11-13 21:03:37 +0100

    Fix double wave spawn at start

--- Assets/Outfoxeed/Scripts/WaveSpawner.cs
+++ Assets/Outfoxeed/Scripts/WaveSpawner.cs
@@ -13,8 +13,6 @@ public class WaveSpawner : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SpawnWave();
-
         InvokeRepeating("SpawnWave", 0f, 5f);
     }
 
