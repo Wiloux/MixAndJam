e7069b2c9848b682802e494b7ce5a144c173a17c
Morigan Letourneau <letourneaumorigan@gmail.com>
2020-11-14 15:58:36 +0100

    commit

--- Assets/Outfoxeed/Scripts/SceneLoader.cs
+++ Assets/Outfoxeed/Scripts/SceneLoader.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class SceneLoader : MonoBehaviour
 {
     public int gameSceneIndex = 1;
+
+    public void LoadGameMenuScene() { GameHandler.instance.SaveHighScore(); SceneManager.LoadScene(0); }
     public void LoadGameScene(){SceneManager.LoadScene(gameSceneIndex);}
 
     public void LoadIndexScene(int index) { SceneManager.LoadScene(index); }
