1f4409617fa68fe9701338e03111f8a73e98f08c
Morigan Letourneau <letourneaumorigan@gmail.com>
2020-11-14 13:26:55 +0100

    New range enemy with 1/4 probability of spawn

---
+++ Assets/Outfoxeed/Scripts/EnemyBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBullet : MonoBehaviour
+{
+    public Vector2 dir;
+    public float spd;
+    public Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = dir * spd;
+    }
+
+    //private void OnTriggerEnter2D(Collider2D collision)
+    //{
+    //    if (collision.tag == "Player")
+    //    {
+    //        // Tuer joueur
+    //    }
+
+    //    if (collision.tag != "Ennemy")
+    //    {
+    //        Destroy(gameObject);
+    //    }
+    //}
+}
---
+++ Assets/Outfoxeed/Scripts/RangeEnemy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RangeEnemy : EnemyAI
+{
+    public GameObject projectilePrefab;
+    private void Awake()
+    {
+        AttackAction = GetPlayerInsight;
+        SecondAction = null;
+    }
+    void GetPlayerInsight()
+    {
+        Vector2 targetDir = (target.position - transform.position).normalized;
+        RaycastHit2D hit;
+        hit = Physics2D.Raycast(transform.position, targetDir, attackRange);
+        if (hit.transform.CompareTag("Player")){Shoot(targetDir);}
+        else ChasePlayer();
+    }
+    void Shoot(Vector2 direction)
+    {
+        Debug.Log("Shoot");
+        animator.SetBool("isAttacking", true);
+        // Set time for next attack
+        SetNextAttackTime();
+
+        GameObject projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        projectile.GetComponent<EnemyBullet>().dir = direction;
+    }
+}
